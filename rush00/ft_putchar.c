41f71f94a0a1e1289862c327063f298f9397714f
Emmanuel <test@nomail.com>
2024-06-09 23:03:27 +0200

    keep trying

--- rush00/ft_putchar.c
+++ rush00/ft_putchar.c
@@ -1,39 +1,7 @@
+
 #include <unistd.h>
 
-void ft_putchar(int w, int h)
+void ft_putchar(char c, int l)
 {
-    // if w=0 or h=0 print a message
-    if (w == 0 || h == 0)
-    {
-        write(1, "0 is not a valid size\n", 23);
-    }
-
-    // if w=1 and h=1
-    if ( w == 1 && h == 1)
-    {
-        char b[2];
-        b[0] = 111; // o
-        b[1] = 10; // \n
-        write(1, &b, 2);
-        return;
-    }
-
-    // top and bottom line
-    char line[w+1];
-    line[0] = 111;
-    int i = 1;
-    while (i < w - 1)
-    {
-        line[i] = '-';
-        i++;
-    }
-    line[w-1] = 111;
-    line[w] = 10;
-    write(1, &line, w + 1);
-}
-
-int main()
-{
-    ft_putchar(1, 1);
-    return 0;
+    write(1, &c, l);
 }
---
+++ rush00/main.c
@@ -0,0 +1,8 @@
+
+void rush(int w, int h);
+
+int main()
+{
+    rush(2, 1);
+    return 0;
+}
